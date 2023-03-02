9b2bb6bcbdf8a185b1c74627f918efd085314c88
hansch <durango185@msn.com>
2023-03-02 14:46:51 +0100

    hi

--- src/hanjatest.java
+++ src/hanjatest.java
@@ -4,6 +4,8 @@ public class hanjatest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello World");
+System.out.println("hello Ece");
+System.out.println("hello Phiiiiiliiiipppppppppp");
 	}
 
 }
