27b16414407a00051ea4f47ec4566cf11631cdf0
philippdunkel <philippdunkel@philipps-air.edu.ds.fhnw.ch>
2023-03-27 12:26:17 +0200

    SpamController ActionEvents hinzugefügt, SpamModel Methoden hinzugefügt

--- src/SpamDetectorV2/SpamController.java
+++ src/SpamDetectorV2/SpamController.java
@@ -1,6 +1,10 @@
 package SpamDetectorV2;
 
-public class SpamController {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
+public class SpamController implements EventHandler<ActionEvent>{
 	//declare view and model
 	private final SpamView view;
 	private final SpamModel model;
@@ -10,5 +14,29 @@ public class SpamController {
 		//reference view and model objects
 		this.model = model;
 		this.view = view;
+		
+		//Action events for Buttons
+		view.addBtn.setOnAction(this::handle);
+		view.deleteBtn.setOnAction(this::handle);
+		view.deleteAllBtn.setOnAction(this::handle);
+		view.checkBtn.setOnAction(this::handle);
+		view.deleteSpamBtn.setOnAction(this::handle);
+	}
+
+	@Override
+	public void handle(ActionEvent e) {
+		Button b = (Button) e.getSource();
+		
+		if (b == view.addBtn) {
+			model.add();
+		} else if (b == view.deleteBtn) {
+			
+		} else if (b == view.deleteAllBtn) {
+			
+		} else if (b == view.checkBtn) {
+			model.checkSpam();
+		} else if (b == view.deleteSpamBtn) {
+			
+		}
 	}
 }
